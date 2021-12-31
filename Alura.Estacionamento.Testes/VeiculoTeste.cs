d053823d0830951ca3ef89891f31e949afe58e01
maanoel <manoelvitorbrito@gmail.com>
2021-12-30 23:35:48 -0300

    Primeira parte concluida

--- Alura.Estacionamento.Testes/VeiculoTeste.cs
+++ Alura.Estacionamento.Testes/VeiculoTeste.cs
@@ -5,25 +5,32 @@ namespace Alura.Estacionamento.Testes
 {
   public class VeiculoTeste
   {
+    //AAA PATTERN
+    //arrange 
+    //Act
+    //Assert 
+
     [Fact]
     public void TestaVeiculoAcelerar()
     {
-      //AAA PATTERN
-      //arrange 
-      //Act
-      //Assert 
+      //Arrange
       var veiculo = new Veiculo();
+      //Act
       veiculo.Acelerar(10);
+      //Assert
       Assert.Equal(100, veiculo.VelocidadeAtual);
     }
 
     //Na totvs, usamos o MSTest para realizar o testes unitários..
     //MSTEXT usa TestMethod e TestClass
     [Fact]
-    public void TestaVeicularFrear() 
+    public void TestaVeicularFrear()
     {
+      //Arrange
       var veiculo = new Veiculo();
+      //Act
       veiculo.Frear(10);
+      //Assert
       Assert.Equal(-150, veiculo.VelocidadeAtual);
     }
   }
