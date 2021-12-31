535b344981f9474dd3ed84c518f978ec7c51d8fc
maanoel <manoelvitorbrito@gmail.com>
2021-12-30 23:31:32 -0300

    teste bßsico

--- Alura.Estacionamento.Testes/VeiculoTeste.cs
+++ Alura.Estacionamento.Testes/VeiculoTeste.cs
@@ -12,5 +12,13 @@ namespace Alura.Estacionamento.Testes
       veiculo.Acelerar(10);
       Assert.Equal(100, veiculo.VelocidadeAtual);
     }
+
+    [Fact]
+    public void TestaVeicularFrear() 
+    {
+      var veiculo = new Veiculo();
+      veiculo.Frear(10);
+      Assert.Equal(-150, veiculo.VelocidadeAtual);
+    }
   }
 }
