b1e23c9d9335e2732531a12db90c10c59217b0a2
maanoel <manoelvitorbrito@gmail.com>
2021-12-30 23:40:21 -0300

    criando teste simples

--- Alura.Estacionamento.Testes/VeiculoTeste.cs
+++ Alura.Estacionamento.Testes/VeiculoTeste.cs
@@ -1,3 +1,4 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
 using Xunit;
 
@@ -32,6 +33,14 @@ namespace Alura.Estacionamento.Testes
       veiculo.Frear(10);
       //Assert
       Assert.Equal(-150, veiculo.VelocidadeAtual);
+    }
+
+    [Fact]
+    public void TestaTipoVeiculo()
+    {
+      var veiculo = new Veiculo();
+
+      Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
     }
   }
 }
