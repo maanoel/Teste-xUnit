2272da89b2fe7566b41c866624ba4211b34fae24
maanoel <manoelvitorbrito@gmail.com>
2022-01-01 19:39:39 -0300

    Criando vßrios parÔmetros para o teste no xUnit usando o InlineData

--- Alura.Estacionamento.Testes/PatioTestes.cs
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -28,5 +28,33 @@ namespace Alura.Estacionamento.Testes
       //assert
       Assert.Equal(2, faturamento);
     }
+
+    //O inline data permite que para o mesmo teste possamos ter diversos dados diferentes
+    [Theory]
+    [InlineData("Vitor brito", "ASD-7777", "PRETO", "GOL")]
+    [InlineData("Nadine brito", "ASB-7777", "PRETO", "GOL")]
+    [InlineData("Dante brito", "ABB-7777", "PRETO", "GOL")]
+
+    public void ValidarFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+    {
+      //Arrange
+      var estacionamento = new Patio();
+      estacionamento.OperadorPatio = new Operador();
+      var veiculo = new Veiculo();
+      veiculo.Proprietario = proprietario;
+      veiculo.Tipo = TipoVeiculo.Automovel;
+      veiculo.Cor = cor;
+      veiculo.Modelo = modelo;
+      veiculo.Placa = placa;
+
+      estacionamento.RegistrarEntradaVeiculo(veiculo);
+      estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+      //Act
+      double faturamento = estacionamento.TotalFaturado();
+      //Assert
+      Assert.Equal(2, faturamento);
+    }
+
   }
 }
